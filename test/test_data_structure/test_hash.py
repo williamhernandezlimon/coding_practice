51e459d88d61c8feffee9832118ed890dd555d51
william <williamhernandezlimon@gmail.com>
2020-08-13 18:35:00 -0700

    Add code for adding two numbers for a target number

---
+++ test/test_data_structure/test_hash.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from src.data_structure.hash import *
+from pytest import mark
+
+
+TEST_TARGET_SUM = [
+	([], 7, None),
+	([1], 7, None),
+	([2,3], 1, None),
+	([2,3], 0, None),
+	([2,2], 4, [2,2]),
+	([1,1,1,1,1], 7, None),
+	([1,2,3,4,5], 7, [2, 5])
+]
+@mark.parametrize("test_l, test_target, expected_response", TEST_TARGET_SUM)
+def test_target_sum(test_l, test_target, expected_response):
+	response = target_sum(test_l, test_target)
+
+	assert response == expected_response
