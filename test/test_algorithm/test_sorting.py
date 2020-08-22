467bff1363fa06d109bf23a5173d38a890f95f5f
william <williamhernandezlimon@gmail.com>
2020-08-22 12:08:21 -0700

    Add quicksort

---
+++ test/test_algorithm/test_sorting.py
@@ -0,0 +1,18 @@
+from src.algorithm import sorting
+from pytest import mark
+
+
+TEST_QUICKSORT = [
+	([], 0, 0, []),
+	([], 1, 2, []),
+	([1, 2, 3], 0, 2, [1, 2, 3]),
+	([2, 1, 3], 2, 0, [2, 1, 3]),
+	([3, 4, 5, 7, 9, 8], 0, 5, [3, 4, 5, 7, 8, 9])
+]
+
+@mark.parametrize("test_list, test_low, test_high, expected_response", TEST_QUICKSORT)
+def test_quicksort(test_list, test_low, test_high, expected_response):
+	sorting.quicksort(test_list, test_low, test_high)
+
+	assert test_list == expected_response
+
