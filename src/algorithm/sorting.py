ee448a992124b7518df338f6df29eb2191b79581
william <williamhernandezlimon@gmail.com>
2020-08-23 13:00:03 -0700

    Add merge sort

--- src/algorithm/sorting.py
+++ src/algorithm/sorting.py
@@ -50,4 +50,44 @@ def quicksort(list, low, high):
 		quicksort(list, low, pivot_position-1)
 		quicksort(list, pivot_position+1, high)
 
-	return list
+
+def mergesort(list):
+	"""
+	Merge sort algorithm
+	list:
+		list of numbers that will be sorted
+	"""
+	if len(list) <= 1:
+		return list if list else []
+
+	# split left and right
+	middle = len(list) // 2
+	left = list[:middle]
+	right = list[middle:]
+	
+
+	# sort left and right
+	mergesort(left)
+	mergesort(right)
+
+
+	# merge left and right
+	i = j = k = 0
+	while i < len(left) and j < len(right):
+		if left[i] < right[j]:
+			list[k] = left[i]
+			i += 1
+		else:
+			list[k] = right[j]
+			j += 1
+		k += 1
+
+	# merge unvisted elements
+	while i < len(left):
+		list[k] = left[i]
+		i += 1
+		k += 1
+	while j < len(right):
+		list[k] = right[j]
+		j += 1
+		k += 1
