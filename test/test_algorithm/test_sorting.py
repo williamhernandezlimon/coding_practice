209f78e6716aaaff697d833f8697df0640b86e2b
william <williamhernandezlimon@gmail.com>
2020-08-23 13:00:19 -0700

    Add merge sort unit-tests

--- test/test_algorithm/test_sorting.py
+++ test/test_algorithm/test_sorting.py
@@ -16,3 +16,18 @@ def test_quicksort(test_list, test_low, test_high, expected_response):
 
 	assert test_list == expected_response
 
+
+TEST_MERGESORT = [
+	([], []),
+	([1], [1]),
+	([1, 1, 1], [1, 1, 1]),
+	([2, 1, 3], [1, 2, 3]),
+	([2, 2, 1, 3], [1, 2, 2, 3]),
+	([2, 2, 1, 1, 3, 3], [1, 1, 2, 2, 3, 3]),
+	([2, 1, 4, 3], [1, 2, 3, 4])
+]
+@mark.parametrize("test_list, expected_response", TEST_MERGESORT)
+def test_mergesort(test_list, expected_response):
+	sorting.mergesort(test_list)
+
+	assert test_list == expected_response
