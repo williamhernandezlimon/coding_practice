d6cad79d1388c1376e9d012fe8dc22e984416c4d
william <williamhernandezlimon@gmail.com>
2020-08-26 18:30:36 -0700

    Add heap sort unit tests

--- test/test_algorithm/test_sorting.py
+++ test/test_algorithm/test_sorting.py
@@ -22,6 +22,20 @@ def test_quick_sort(test_list, test_low, test_high, expected_response):
 #	Reassigning TEST_SORT allows test_list to be reset
 #	Since every test method modifies TEST_SORT's test_list by reference
 TEST_SORT = [
+	# TODO: add unit-tests for duplicate elements
+	([], []),
+	([1], [1]),
+	([2, 1, 3], [1, 2, 3]),
+	([2, 1, 4, 3], [1, 2, 3, 4]),
+	([5, 2, 1, 6, 4, 3], [1, 2, 3, 4, 5, 6])
+]
+@mark.parametrize("test_list, expected_response", TEST_SORT)
+def test_heap_sort(test_list, expected_response):
+	sorting.heap_sort(test_list)
+
+	assert test_list == expected_response
+
+TEST_SORT = [
 	([], []),
 	([1], [1]),
 	([1, 1, 1], [1, 1, 1]),
@@ -31,8 +45,8 @@ TEST_SORT = [
 	([2, 1, 4, 3], [1, 2, 3, 4])
 ]
 @mark.parametrize("test_list, expected_response", TEST_SORT)
-def test_merge_sort(test_list, expected_response):
-	sorting.merge_sort(test_list)
+def test_insertion_sort(test_list, expected_response):
+	sorting.insertion_sort(test_list)
 
 	assert test_list == expected_response
 
@@ -47,8 +61,8 @@ TEST_SORT = [
 	([2, 1, 4, 3], [1, 2, 3, 4])
 ]
 @mark.parametrize("test_list, expected_response", TEST_SORT)
-def test_insertion_sort(test_list, expected_response):
-	sorting.insertion_sort(test_list)
+def test_merge_sort(test_list, expected_response):
+	sorting.merge_sort(test_list)
 
 	assert test_list == expected_response
 
