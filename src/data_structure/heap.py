689912a46e160a38986ddeaa817d16c5dd3a6db4
william <williamhernandezlimon@gmail.com>
2020-09-29 23:27:32 -0700

    Add kth smallest element

--- src/data_structure/heap.py
+++ src/data_structure/heap.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
+def extract(a):
+	"""
+	Return the root and re-heapify
+	"""
+	if len(a) <= 0: return None
+	root = a[0]
+
+	# swap root with last element
+	a[0], a[len(a)-1] = a[len(a)-1], a[0]
+
+	# truncate last element
+	a = a.pop()
+
+	return root
+
+
 def heap_max(a):
 	"""
 	Builds a max heap using the existing list "a"
@@ -31,7 +47,35 @@ def heap_min(a):
 		_heapify_min(a, i)
 
 
+def kth_smallest_element(a, k):
+	"""
+	Find the kth smallest element from list a
+	a:
+		list of integers
+	k:
+		the kth smallest element we're supposed to find
+	return:
+		kth smallest element
+	complexity:
+		time: O(nlog(n))
+		space: O(1)
+	"""
+	kth_smallest = None
+	heap_min(a)
+	for i in range(k, 0, -1):
+
+		kth_smallest  = extract(a)
+		heap_min(a)
+
+
+	return kth_smallest
+
+
 def _heapify_max(a, parent_pos):
+	"""
+	complexity:
+		time: O(log(n))
+	"""
 	left = (parent_pos * 2) + 1
 	right = (parent_pos * 2) + 2
 
@@ -45,12 +89,15 @@ def _heapify_max(a, parent_pos):
 
 	# update if parent is not largest and heapify
 	if max_pos != parent_pos:
-		print(f"swapping a[max_pos]: {a[max_pos]} a[parent_pos]: {a[parent_pos]}")
 		a[max_pos], a[parent_pos] = a[parent_pos], a[max_pos]
 		_heapify_max(a, max_pos)
 
 
 def _heapify_min(a, parent_pos):
+	"""
+	complexity:
+		time: O(log(n))
+	"""
 	left = (2*parent_pos) + 1 if (2*parent_pos) + 1 < len(a) else None
 	right = (2*parent_pos) + 2 if (2*parent_pos) + 2 < len(a) else None
 	min_pos = parent_pos
