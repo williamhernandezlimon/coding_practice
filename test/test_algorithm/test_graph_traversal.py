d6382157d3b640bcf7074dea67078cf884023230
william <williamhernandezlimon@gmail.com>
2020-10-13 11:42:25 -0700

    Add dijkstras algorithm unit tests

--- test/test_algorithm/test_graph_traversal.py
+++ test/test_algorithm/test_graph_traversal.py
@@ -15,6 +15,20 @@ def create_graph():
 		"h": ["e", "g"],
 		"s": ["c", "g"]
 	}
+TEST_GRAPH_1 = {
+	'a': {'b': 1}, 
+	'b': {'c': 2, 'b': 5}, 
+	'c': {'d': 1}, 
+	'd': {}
+}
+TEST_GRAPH_2 = {
+	'a': {'b': 14, 'c': 9, 'd': 7},
+	'b': {'a': 14, 'c': 2, 'e': 9},
+	'c': {'a': 9, 'b': 2, 'd': 10, 'f': 11},
+    'd': {'a': 7, 'c': 10, 'f': 15},
+    'e': {'b': 9, 'f': 6},
+    'f': {'c': 11, 'd': 15, 'e': 6}
+}
 
 def test_bfs():
 	graph = create_graph()
@@ -35,3 +49,16 @@ def test_dfs():
 	assert iterative_response == expected_iterative_response
 	assert iterative_efficient_response == expected_iterative_response
 	assert recursive_response == expected_recursive_response
+
+
+TEST_DIJKSTRAS = [
+	({}, None, None, None),
+	(TEST_GRAPH_1, 'a', {'a': 0, 'c': 3, 'b': 1, 'd': 4}, {'b': 'a', 'c': 'b', 'd': 'c'}),
+	(TEST_GRAPH_2, 'a', {'a': 0, 'c': 9, 'b': 11, 'e': 20, 'd': 7, 'f': 20}, {'b': 'c', 'c': 'a', 'd': 'a', 'f': 'c', 'e': 'b'}),
+]
+@mark.parametrize("test_graph, test_start, expected_response_visited, expected_response_path", TEST_DIJKSTRAS)
+def test_dijkstras(test_graph, test_start, expected_response_visited, expected_response_path):
+	response_visited, response_path = dijkstras(test_graph, test_start)
+
+	assert response_visited == expected_response_visited
+	assert response_path == expected_response_path
