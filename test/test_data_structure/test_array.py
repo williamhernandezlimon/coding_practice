bef0ab8377573253c7f830647d391c3b53871417
william <williamhernandezlimon@gmail.com>
2021-03-19 15:28:32 -0700

    Update to fix search method more unit tests

--- test/test_data_structure/test_array.py
+++ test/test_data_structure/test_array.py
@@ -505,8 +505,10 @@ def test_remove_duplicates(test_array, expected_response):
 
 @mark.parametrize("test_nums, test_target, expected_response", [
 		([], 0, -1),
+		([1,3], 3, 1),
+		([3,4,5,6,7,8,1,2], 2, 7),
 		([2], 3, -1),
-		([2,1], 0, 1),
+		([2,1], 0, -1),
 		([4,5,6,7,0,1,2], 0, 4)
 	]
 )
