788251418877b65a35c932743f2923c016cbb6cd
william <williamhernandezlimon@gmail.com>
2021-03-20 00:19:59 -0700

    Add divide method

--- test/test_data_structure/test_hash.py
+++ test/test_data_structure/test_hash.py
@@ -3,6 +3,25 @@ from src.data_structure.hash import *
 from pytest import mark
 
 
+@mark.parametrize("test_dividend, test_divisor, expected_response", [
+		(0, 3, 0),
+		(3, 0, None),
+		(1, 3, 0),
+		(-1, 3, 0),
+		(3, 1, 3),
+		(10, 3, 3),
+		(10, -3, -3),
+		(-10, 3, -3),
+		(-10, -3, 3),
+		(10, 3, 3),
+	]
+)
+def test_divide(test_dividend, test_divisor, expected_response):
+	response = divide(test_dividend, test_divisor)
+
+	assert response == expected_response
+
+
 @mark.parametrize(
 	"test_l, test_target, expected_response", [
 		([], 7, None),
