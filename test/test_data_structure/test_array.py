1a09c6db7b4c60fb1313f00d82c8d3d16d48051b
william <williamhernandezlimon@gmail.com>
2021-03-22 15:10:37 -0700

    Add matrix rotation unit tests

--- test/test_data_structure/test_array.py
+++ test/test_data_structure/test_array.py
@@ -503,6 +503,19 @@ def test_remove_duplicates(test_array, expected_response):
 	assert response == expected_response
 
 
+@mark.parametrize("test_matrix, expected_response", [
+		([[]], [[]]),
+		([[1]], [[1]]),
+		([[1,2],[3,4]], [[3,1],[4,2]]),
+		([[1,2,3],[4,5,6],[7,8,9]], [[7,4,1],[8,5,2],[9,6,3]]),
+		([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]], [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]])
+	]
+)
+def test_rotate(test_matrix, expected_response):
+	rotate(test_matrix)
+
+	assert test_matrix == expected_response
+
 @mark.parametrize("test_nums, test_target, expected_response", [
 		([], 0, -1),
 		([1,3], 3, 1),
