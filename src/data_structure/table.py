ce40ac0021fa90e5e1ea68a55f93da6a9ed0c5c5
william <williamhernandezlimon@gmail.com>
2021-04-13 10:48:39 -0700

    Add spiral order matrix

--- src/data_structure/table.py
+++ src/data_structure/table.py
@@ -36,3 +36,56 @@ def set_zeros(matrix):
 		for column in range(len(matrix[row])):
 			if row in rows_to_zero or column in columns_to_zero:
 				matrix[row][column] = 0
+
+
+def spiral_order(matrix):
+	"""
+	Given m x n matrix, return all elements of the matrix 
+	in spiral order
+	matrix:
+		list of list of integers
+	return:
+		list of integers in spiral order
+	complexity:
+		time: O(N+M)
+		space: O(N+M)
+	"""
+	order = []
+	t, b, l, r = 0, len(matrix)-1, 0, len(matrix[0])-1
+	while t <= b and l <= r:
+		print(f"t: {t} b: {b} l: {l} r: {r}")
+		# go right
+		for i in range(t, r+1):
+			if matrix[t][i] != None:
+				order.append(matrix[t][i])
+				matrix[t][i] = None
+		# update top
+		t += 1
+
+		# go down
+		for i in range(t, b+1):
+			if matrix[i][r] != None:
+				order.append(matrix[i][r])
+				matrix[i][r] = None
+		# update right
+		r -= 1
+
+		# go left
+		print(f"going left r: {r} l: {l} b: {b}")
+		for i in range(r, l-1, -1):
+			if matrix[b][i] != None:
+				order.append(matrix[b][i])
+				matrix[b][i] = None
+		# update bottom
+		b -= 1
+
+		# go up
+		for i in range(b, t-1, -1):
+			if matrix[i][l] != None:
+				order.append(matrix[i][l])
+				matrix[i][l] = None
+		# update left
+		l += 1
+
+	return order
+
